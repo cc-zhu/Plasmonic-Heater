634c7bb4c981980bece3c9fd704ed4a663e95e17
cicizhu <cici.zhu116@gmail.com>
2026-07-14 16:02:44 -0400

    no message

--- Pt Thin Film/pt_absorption_model.m
+++ Pt Thin Film/pt_absorption_model.m
@@ -1,51 +1,57 @@
-%absorption of 80nm pt film using experimental refractive index
+%analytical absorption model - platinum
 
 clear;
 
-%dielectric function from experimental data
-
-%retrieve experimental data (tselin 80nm)
-fileID = fopen('Datasets/Tselin-80nm-n.txt');
-tselin_80nm_n = fscanf(fileID, '%f%f', [2 2000]);
-fclose(fileID);
-
-fileID = fopen('Datasets/Tselin-80nm-k.txt');
-tselin_80nm_k = fscanf(fileID, '%f%f', [2 2000]);
-fclose(fileID);
-
-exp_eps = zeros([1 2000]);   %dielectric constant container
+%dielectric function from Drude-Lorentz model
+%number of elements
+n = 500;
 
 %constant definitions
+eps0 = 8.854e-12;   %vacuum permittivity
 c = 3e8;   %speed of light
-ep0 = 8.85419e-12;   %vacuum permittivity
-mu0 = 4*pi*1e-7;   %vacuum permeability
+h = 6.626e-34;   %planck's constant (J*s)
+hbar = h/(2*pi);   %reduced planck's constant (J*s)
+ev_to_j = 1.602e-19;   %eV to J conversion factor
+epsinf = 1.76;   %dielectric at infinite frequency
 
-%convert refractive indices to dielectric constants
-for i = 1:2000
-    lambdai = tselin_80nm_n(1,i) * 1e-6;   %convert μm to m
-    omegai = 2*pi*c/lambdai;
+%Drude parameters
+h_Omegap_ev = 7.26;   %plasma frequency (eV)
+Omegap = h_Omegap_ev * ev_to_j / hbar;
 
-    N = tselin_80nm_n(2,i) + 1i*tselin_80nm_k(2,i);   %N = n + ik
-    eps = N^2;
-    exp_eps(1,i) = eps;
-end
+h_Gamma_ev = 0.046;   %relaxation constant (eV)
+Gamma = h_Gamma_ev * ev_to_j / hbar;
 
-
-%absorption calculation
-
-%number of elements
-n = 2000;
-
-%incident angle lower bound
-t1 = 55;
+%Lorentz parameters
+omegaj = [0.831, 1.471, 3.449, 10.461].*ev_to_j./hbar;   %resonance frequency
+Omegaj = [6.761, 6.895, 9.452, 15.473].*ev_to_j./hbar;   %strength
+gammaj = [0.879, 2.052, 5.424, 6.286].*ev_to_j./hbar;   %relaxation constant
 
 %independent variables
+t1 = 60;   %incident angle lower bound
 thetai = linspace(t1, 90, n);   %incident angle (in degrees) on x-axis, linear scale
 lambda = linspace(0.19, 3.5, n);   %wavelengths 0.19 to 3.5 μm -> converted to m later
 [T,L] = meshgrid(thetai, lambda);   %plotting coordinates
 
-A = zeros(n);   %container for absorption values
+%containers
+Epsf = zeros([1 n]);   %dielectric function of film
 
+for i = 1:n
+    lambdai = lambda(1,i) * 1e-6;   %convert μm to m
+    omega = 2*pi*c/lambdai;
+    
+    %Lorentz oscillators
+    lo = 0;
+    for j = 1:4
+        lo = lo + Omegaj(1,j)^2 / (omegaj(1,j)^2 - omega^2 - 1i*gammaj(1,j)*omega);
+    end
+
+    %Drude-Lorentz
+    Epsf(1,i) = epsinf - Omegap^2 / (omega^2 + 1i*Gamma*omega) + lo;
+end
+
+
+%absorption calculation
+A = zeros(n);   %container for absorption values
 df = 80e-9;   %film thickness 80 nm
 
 %transfer matrix method
@@ -69,7 +75,7 @@ for i = 1:n
         thetaij = deg2rad(T(i,j));   %current thetai
         lambdaij = L(i,j) * 1e-6;   %current lambda, convert μm to m
         omegaij = 2*pi*c/lambdaij;
-        epsf = exp_eps(1,i);
+        epsf = Epsf(1,i);
 
         %reflection & transmission coefficients
         rij = r(df, omegaij, thetaij, epsf);
